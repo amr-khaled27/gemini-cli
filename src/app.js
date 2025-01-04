a1507fe069037b1b27f1e8838f5f1211b998643f
amr-khaled27 <3moorkh2005@gmail.com>
2025-01-04 14:15:01 +0200

    updated version to alpha

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ async function app() {
 }
 
 program
-.version('0.0.7')
+.version('0.0.7-alpha')
 .description('Chat with Gemini from the comfort of your terminal!')
 .option('-m, --message <prompt>', 'Send a prompt instantly to Gemini.')
 .option('<no options>', 'Enter chat mode with gemini.')
